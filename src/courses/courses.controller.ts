d154d59b70ad6c4504dada5bb7a8193dfe4c3556
hmyunis <hamdim801@gmail.com>
2024-01-30 17:15:53 +0300

    Course creation works now.

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Render, Res } from '@nestjs/common';
 import * as fs from 'fs';
 
 @Controller('courses')
@@ -12,6 +12,33 @@ export class CoursesController {
       numberOfCourses: user.courses.length,
     });
   }
+
+  @Post('/:username/new')
+  addNewCourse(@Body() body, @Param('username') username, @Res() res) {
+    const readUser = JSON.parse(JSON.stringify(getUserbyUsername(username)));
+      if (!courseExists(body.courseCode, username)) {
+        readUser.courses.push(body);
+        console.log('Course has been appended successfully.');
+        overwriteFile(readUser, username);
+      } else {
+        console.log(
+          `Course ${body.courseCode} already exists.`,
+        );
+      }
+    return res.redirect(`/courses/${username}`);
+  }
+
+  @Get('/:username/new')
+  @Render('modals/addNewCourse')
+  getNewCourseAddingPage(@Param('username') username) {
+    const user = getUserbyUsername(username);
+    return {
+      username,
+      arrayOfCourses: user.courses,
+      numberOfCourses: user.courses.length,
+    };
+  }
+
 }
 
 function getUserbyUsername(username: string) {
@@ -27,5 +54,43 @@ function getUserbyUsername(username: string) {
     return null;
   } catch (error) {
     console.error('Error reading or parsing file:', error);
+  }
+}
+
+function courseExists(courseCode: string, username: string) {
+  try {
+    const data = fs.readFileSync(process.env.FILE_PATH, 'utf-8');
+    const readJson = JSON.parse(data);
+    for (let i = 0; i < Object.keys(readJson).length; i++) {
+      const user = readJson[i][`user_${i + 1}`];
+      if (
+        user.accountDetails.username.trim() === username.trim() &&
+        user.courses.courseCode.trim() === courseCode.trim()
+      ) {
+        return true;
+      }
+    }
+    return false;
+  } catch (error) {
+    console.error();
+  }
+}
+
+function overwriteFile(newUserObj, username: string) {
+  try {
+    const data = fs.readFileSync(process.env.FILE_PATH, 'utf-8');
+    const readJson = JSON.parse(data);
+    for (let i = 0; i < Object.keys(readJson).length; i++) {
+      const user = readJson[i][`user_${i + 1}`];
+      if (user.accountDetails.username.trim() === username.trim()) {
+        readJson[i][`user_${i + 1}`] = JSON.parse(JSON.stringify(newUserObj));
+        const jsonString = JSON.stringify(readJson, null, 2);
+        fs.writeFileSync(process.env.FILE_PATH, jsonString);
+        console.log('Data has been successfully overwritten.');
+        break;
+      }
+    }
+  } catch (error) {
+    console.error('Error writing file:', error);
   }
 }
--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,20 +1,8 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
-import { Task } from './dtos/create-task.dto';
 
 @Controller('dashboard')
 export class DashboardController {
     constructor(private dashboardService: DashboardService){}
 
-    @Get()
-    @Render('dashboard')
-    serveDashboardPage(){
-        return this.dashboardService.loadDashboard();
-    }
-
-    @Post('/new')
-    @Render('dashboard-new-task')
-    createTask(@Body() body: Task){
-        return this.dashboardService.createTask(body);
-    }
 }
